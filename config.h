e450be25b5e69806278a50abd55c5691664d1132
Lorenc Rudolph <r.lorenc@2Gis.ru>
2019-12-17 21:21:05 +0700

    Remove unnecessary keymaps

--- config.h
+++ config.h
@@ -5,8 +5,7 @@
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
+                                  SONG(PLANCK_SOUND) \
                                 }
 #endif
 
