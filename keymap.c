df336bc5a3577872e56b078de6d6ea6e674dec17
Rudolph Lorenz <rudlorenz@gmail.com>
2021-01-17 17:00:04 +0700

    Add clion macro.

--- keymap.c
+++ keymap.c
@@ -41,6 +41,7 @@ enum planck_keycodes
 	BACKLIT,
 	CLION1, // send shift+alt+7
 	CLION2, // send ctrl+alt+home
+	CLION3, // send alt+insert
 };
 
 #define LOWER MO(_LOWER)
@@ -93,14 +94,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │   #  │  \   │ PgUp │ PgDn │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │CLION2│      │      │CLION1│      │      │      │      │ Next │ Vol- │ Vol+ │ Play │
+ * │CLION2│CLION3│      │CLION1│      │      │      │      │ Next │ Vol- │ Vol+ │ Play │
  * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_RAISE] = LAYOUT_planck_grid(
 	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 	KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
 	_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-	CLION2,  _______, _______, CLION1,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+	CLION2,  CLION3,  _______, CLION1,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* MOBA
@@ -265,6 +266,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 			SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LALT) SS_TAP(X_HOME) SS_UP(X_LCTRL) SS_UP(X_LALT));
 		}
 		break;
+
+	case CLION3:
+		if (record->event.pressed)
+		{
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_INSERT) SS_UP(X_LALT));
+		}
+		break;
+
 	}
 	return true;
 }
--- rules.mk
+++ rules.mk
@@ -1,3 +1,4 @@
 SRC += muse.c
 AUDIO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
+EXTRAKEY_ENABLE = yes
