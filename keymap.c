8909c66f9de2331ea4056e08779a52cca10e6a77
Rudolph Lorenz <rudlorenz@gmail.com>
2021-01-17 17:16:03 +0700

    Remove unused layers. Rearrange media keys.

--- keymap.c
+++ keymap.c
@@ -27,17 +27,12 @@ enum planck_layers
 	_QWERTY,
 	_LOWER,
 	_RAISE,
-	_GAME,
-	_MOBA,
 	_ADJUST
 };
 
 enum planck_keycodes
 {
 	QWERTY = SAFE_RANGE,
-	GAME,
-	MOBA,
-	EXT_GAME,
 	BACKLIT,
 	CLION1, // send shift+alt+7
 	CLION2, // send ctrl+alt+home
@@ -92,52 +87,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │  Del │  F1  │  F2  │  F3  │  F4  │  F5  │  F6  │  -   │  =   │  [   │  ]   │  \   │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │   #  │  \   │ PgUp │ PgDn │      │
+ * │      │  F7  │  F8  │  F9  │  F10 │  F11 │  F12 │   #  │  \   │ MStop│ MPlay│      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │CLION2│CLION3│      │CLION1│      │      │      │      │ Next │ Vol- │ Vol+ │ Play │
+ * │CLION2│CLION3│      │CLION1│      │      │      │      │ MPrev│ Vol- │ Vol+ │ MNext│
  * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_RAISE] = LAYOUT_planck_grid(
 	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 	KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-	_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-	CLION2,  CLION3,  _______, CLION1,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
-),
-
-/* MOBA
- * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
- * │ Tab  │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │ EXIT │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Ctrl │   Q  │   W  │   E  │   R  │   T  │   P  │ Null │ Null │ Null │ Null │ Null │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Null │ Alt  │   A  │   S  │   D  │   F  │   V  │   M  │ Null │ Null │ Null │ Null │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Shft │ Esc  │ Alt  │ Gui  │Lower │    Space    │Raise │ Null │ Null │ Null │ Null │
- * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
- */
-[_MOBA] = LAYOUT_planck_grid(
-	KC_TAB,  KC_1,    KC_2,   KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,  KC_9,  KC_0,  EXT_GAME,
-	KC_LCTL, KC_Q,    KC_W,   KC_E,    KC_R,  KC_T,   KC_P,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-	KC_NO,   KC_LALT, KC_A,   KC_S,    KC_D,  KC_F,   KC_V,   KC_M,  KC_NO, KC_NO, KC_NO, KC_NO,
-	KC_LSFT, KC_ESC, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_NO, KC_NO, KC_NO, KC_NO
-),
-
-/* GAME
- * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
- * │ Esc  │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │ EXIT │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Tab  │   Q  │   W  │   E  │   R  │   T  │   Y  │   U  │   I  │   O  │   P  │ Bksp │
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Shft │   A  │   S  │   D  │   F  │   G  │   H  │   J  │   K  │   L  │   ;  │SFTENT│
- * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │ Ctrl │   Z  │   X  │   C  │ Alt  │    Space    │Raise │ Left │ Down │  Up  │ Right│
- * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
- */
-[_GAME] = LAYOUT_planck_grid(
-	KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    EXT_GAME,
-	KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-	KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_SFTENT,
-	KC_LCTRL, KC_Z,   KC_X,    KC_C,  KC_LALT, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+	_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_MSTP, KC_MPLY, _______,
+	CLION2,  CLION3,  _______, CLION1,  _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 /* ADJUST
@@ -148,15 +107,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │      │Voice-│Voice+│Mu On │Mu Off│Mi On │Mi Off│TermOn│TermOf│      │      │      │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │      │      │      │      │PrtScn│PrtScn│      │      │      │ MOBA │ GAME │
+ * │      │      │      │      │      │PrtScn│PrtScn│      │      │      │      │      │
  * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
  */
 [_ADJUST] = LAYOUT_planck_grid(
 	_______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
 	_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______, _______,
 	_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-	_______, _______, _______, _______, _______, KC_PSCR, KC_PSCR, _______, _______,  _______, MOBA   , GAME
-),
+	_______, _______, _______, _______, _______, KC_PSCR, KC_PSCR, _______, _______,  _______, _______, _______
+	),
 };
 // clang-format on
 
@@ -180,54 +139,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		{
 			dprint("mode just switched to qwerty and this is a huge string\n");
 			set_single_persistent_default_layer(_QWERTY);
-		}
-		return false;
-		break;
-
-	case GAME:
-		if (record->event.pressed)
-		{
-			dprint("GAME mode activated\n");
-#ifdef AUDIO_ENABLE
-			stop_all_notes();
-			PLAY_SONG(rise_up_song);
-#endif
-			layer_on(_GAME);
-		}
-		return false;
-		break;
-
-	case MOBA:
-		if (record->event.pressed)
-		{
-			dprint("MOBA layer activated\n");
-#ifdef AUDIO_ENABLE
-			stop_all_notes();
-			PLAY_SONG(plover_song);
-#endif
-
-			layer_on(_MOBA);
-		}
-		return false;
-		break;
-
-	case EXT_GAME:
-		if (record->event.pressed)
-		{
-			dprint("gaming layers deactivated\n");
-#ifdef AUDIO_ENABLE
-			stop_all_notes();
-			PLAY_SONG(plover_gb_song);
-#endif
-
-			if (layer_state_is(_GAME))
-			{
-				layer_off(_GAME);
-			}
-			if (layer_state_is(_MOBA))
-			{
-				layer_off(_MOBA);
-			}
 		}
 		return false;
 		break;
